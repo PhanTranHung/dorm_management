acccc5bac69859d6a9f97ce3dd5ac2ed92ad7444
hungphannapaglobal <phan.hung@napaglobal.com>
2021-01-21 21:12:53 +0700

    update Admin UI

--- routes/admin.js
+++ routes/admin.js
@@ -2,8 +2,28 @@ const express = require('express');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.render('admin/index', { page: 'login' });
+router.get('/bill', function (req, res, next) {
+  res.render('admin/index', { page: 'bill' });
+});
+
+router.get('/rooms', function (req, res, next) {
+  res.render('admin/index', { page: 'rooms' });
+});
+
+router.get('/student-feedback', function (req, res, next) {
+  res.render('admin/index', { page: 'student-feedback' });
+});
+
+router.get('/students', function (req, res, next) {
+  res.render('admin/index', { page: 'students' });
+});
+
+router.get('/login', function (req, res, next) {
+  res.render('admin/login');
+});
+
+router.get(['/', '/dashboard'], function (req, res, next) {
+  res.render('admin/index', { page: 'dashboard' });
 });
 
 module.exports = router;
